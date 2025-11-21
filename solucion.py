709b37bdb0a955c546dc1c461aeaadb8a50787b0
layewav <a224206498@unison.mx>
2025-11-21 14:54:45 -0700

    Fix range in growing triangle loop

--- solucion.py
+++ solucion.py
@@ -7,7 +7,7 @@ def triangulo_simetrico(m: int, s: str) -> str:
         return
         # TODO: implementar la lógica para generar el triángulo simétrico en ASCII
         #Parte 1: Triángulo Creciente
-    for i in range(1, m, +1):
+    for i in range(1, m +1):
         print(s*i)
         #Parte 2: Triángulo Decreciente
     for i in range(m -1, 0, -1):
