68e6f0bc106776afb1b65ae3baf2610b5538c179
layewav <a224206498@unison.mx>
2025-11-21 13:59:58 -0700

    Implement height validation and triangle generation
    Added validation for height and implemented logic to generate symmetric triangle in ASCII.

--- solucion.py
+++ solucion.py
@@ -1,6 +1,14 @@
 # En este archivo debes implementar la función
 
 def triangulo_simetrico(m: int, s: str) -> str:
-    # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
-    
-    # TODO: implementar la lógica para generar el triángulo simétrico en ASCII
+        # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
+    if m<=0:
+        print('Error: La altura debe ser un entero postivo')
+        return
+        # TODO: implementar la lógica para generar el triángulo simétrico en ASCII
+        #Parte 1: Triángulo Creciente
+    for i in range(1, m, +1):
+        print(s*i)
+        #Parte 2: Triángulo Decreciente
+    for i in range(m -1, 0, -1):
+        print(s*i)
