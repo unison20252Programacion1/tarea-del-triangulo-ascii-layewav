44adba7791a1000d65ad93b306abd6e4b458f2dd
layewav <a224206498@unison.mx>
2025-11-21 14:11:01 -0700

    Correct spacing in height validation condition
    Fix spacing in conditional check for height validation.

--- solucion.py
+++ solucion.py
@@ -2,7 +2,7 @@
 
 def triangulo_simetrico(m: int, s: str) -> str:
         # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
-    if m<=0:
+    if m<= 0:
         print('Error: La altura debe ser un entero postivo')
         return
         # TODO: implementar la lógica para generar el triángulo simétrico en ASCII
